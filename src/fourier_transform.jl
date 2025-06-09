87cd56336cb28c909707c9246c3ac51aa378cc9b
Lorenzo Monacelli <mesonepigreco@gmail.com>
2025-06-09 12:35:02 +0200

    Added the fourier transform for the symmetries

--- src/fourier_transform.jl
+++ src/fourier_transform.jl
@@ -1,3 +1,4 @@
+
 @doc raw"""
     vector_r2q!(
         v_q :: Array{Complex{T}, 3},
@@ -14,11 +15,11 @@ $$
 
 ## Parameters
 
-- v_q : (n_configs, n_modes, nq) 
+- v_q : (n_configs, 3nat, nq) 
     The target vector in Fourier space.
-- v_sc : (n_configs, n_modes)
+- v_sc : (n_configs, 3*nat_sc)
     The original vector in real space
-- q_tot : (n_dims, nq)
+- q_tot : (3, nq)
     The list of q vectors
 - itau : (nat_sc)
     The correspondance for each atom in the supercell with the atom in the primitive cell.
@@ -35,8 +36,7 @@ function vector_r2q!(
 
     nq = size(q, 2)
     n_random = size(v_sc, 1)
-    n_dims = size(q, 1)
-    nat_sc = size(v_sc, 2) ÷ n_dims
+    nat_sc = size(v_sc, 2) ÷ 3
     nat = size(v_q, 2)
 
     v_q .= 0
@@ -46,9 +46,9 @@ function vector_r2q!(
             @views q_dot_R = q[:, jq]' * R_lat[:, k]
             exp_value = exp(- 1im * 2π * q_dot_R)
 
-            for α in 1:n_dims
-                index_sc = n_dims * (k - 1) + α
-                index_uc = n_dims * (itau[k] - 1) + α
+            for α in 1:3
+                index_sc = 3 * (k - 1) + α
+                index_uc = 3 * (itau[k] - 1) + α
                 @simd for i ∈ 1:n_random
                     v_q[i, index_uc, jq] += exp_value * v_sc[i, index_sc]
                 end
@@ -99,9 +99,8 @@ function vector_q2r!(
 
     nq = size(q, 2)
     n_random = size(v_sc, 1)
-    n_dims = size(R_lat, 1)
-    nat_sc = size(v_sc, 2) ÷ n_dims
-    tmp_vector = zeros(Complex{T}, (n_random, n_dims*nat_sc))
+    nat_sc = size(v_sc, 2) ÷ 3
+    tmp_vector = zeros(Complex{T}, (n_random, 3*nat_sc))
 
     v_sc .= 0
     for jq ∈ 1:nq
@@ -109,9 +108,9 @@ function vector_q2r!(
             @views q_dot_R = q[:, jq]' * R_lat[:, k]
             exp_value = exp(1im * 2π * q_dot_R)
 
-            for α in 1:n_dims
-                index_sc = n_dims * (k - 1) + α
-                index_uc = n_dims * (itau[k] - 1) + α
+            for α in 1:3
+                index_sc = 3 * (k - 1) + α
+                index_uc = 3 * (itau[k] - 1) + α
                 @simd for i ∈ 1:n_random
                     tmp_vector[i, index_sc] += exp_value * v_q[i, index_uc, jq]
                 end
@@ -121,5 +120,143 @@ function vector_q2r!(
 
     v_sc .= real(tmp_vector)
     v_sc ./= √(nq)
+end
+
+@doc raw"""
+    matrix_r2q!(
+        matrix_q :: Array{Complex{T}, 3},
+        matrix_r :: AbstractMatrix{T},
+        q :: Matrix{T},
+        itau :: Vector{I},
+        R_lat :: Matrix{T})
+
+Fourier transform a matrix from real to q space
+
+$$
+M_{ab}(\vec q) = \sum_{\vec R} e^{2\pi i \vec q\cdot \vec R}\Phi_{a;b + \vec R}
+$$
+
+Where ``\Phi_{ab}`` is the real space matrix, the ``b+\vec R`` indicates the corresponding atom in the supercell displaced by ``\vec R``. 
+
+
+## Parameters
+
+- matrix_q : (3nat, 3nat, nq) 
+    The target matrix in Fourier space.
+- matrix_r : (3*nat_sc, 3*nat)
+    The original matrix in real space (supercell)
+- q_tot : (3, nq)
+    The list of q vectors
+- itau : (nat_sc)
+    The correspondance for each atom in the supercell with the atom in the primitive cell.
+- R_lat : (3, nat_sc)
+    The origin coordinates of the supercell in which the corresponding atom is
+"""
+function matrix_r2q!(
+        matrix_q :: Array{Complex{T}, 3},
+        matrix_r :: AbstractMatrix{T},
+        q :: Matrix{T},
+        itau :: Vector{I},
+        R_lat :: Matrix{T}; buffer = default_buffer())
+    nq = size(q, 2)
+    ndims = size(q, 1)
+    nat_sc = size(matrix_r, 1) ÷ ndims
+    nat = size(matrix_q, 1) ÷ ndims
+
+    matrix_q .= T(0.0) 
+
+    @no_escape buffer begin
+        ΔR⃗ = @alloc(T, ndim)
+
+        phase_i = Complex(T)(-2π * 1im)
+
+        for iq in 1:nq
+            for k_i in 1:nat
+                k = ndims*(k_i - 1) + k_α
+                @simd for h_i in 1:nat_sc
+                    @views ΔR⃗ .= R_lat[:, k_i]
+                    @views ΔR⃗ .-= R_lat[:, h_i]
+                    @views q_dot_R = ΔR⃗' * q[:, iq]
+
+                    h_i_uc = itau[h_i]
+
+                    exp_factor = exp(phase_i * q_dot_R)
+                    @views matrix_q[ndims*(h_i_uc - 1) : ndims * h_i_uc, ndims*(k_i - 1) : ndims*k_i, iq] .= matrix_r[ndims*(h_i - 1) : ndims * h_i, ndims*(k_i - 1) : ndims*k_i, iq]
+                    matrix_q[ndims*(h_i_uc - 1) : ndims * h_i_uc, ndims*(k_i - 1) : ndims*k_i, iq] .*= exp_factor
+                end
+            end
+        end
+        nothing
+    end
+end
+
+@doc raw"""
+    matrix_q2r!(
+        matrix_r :: AbstractMatrix{T},
+        matrix_q :: Array{Complex{T}, 3},
+        q :: Matrix{T},
+        itau :: Vector{I},
+        R_lat :: Matrix{T})
+
+Fourier transform a matrix from q space into r space
+
+$$
+\Phi_{ab} = \frac{1}{N_q} \sum_{\vec q}
+M_{ab}(\vec  q) e^{2i\pi \vec q\cdot[\vec R(a) - \vec R(b)]}
+$$
+
+Where ``\Phi_{ab}`` is the real space matrix, ``M_{ab}(\vec q)`` is the q space matrix.
+
+
+## Parameters
+
+
+- matrix_r : (3*nat_sc, 3*nat)
+    The target matrix in real space (supercell)
+- matrix_q : (3nat, 3nat, nq) 
+    The original matrix in Fourier space.
+- q_tot : (3, nq)
+    The list of q vectors
+- itau : (nat_sc)
+    The correspondance for each atom in the supercell with the atom in the primitive cell.
+- R_lat : (3, nat_sc)
+    The origin coordinates of the supercell in which the corresponding atom is
+"""
+function matrix_r2q!(
+        matrix_q :: Array{Complex{T}, 3},
+        matrix_r :: AbstractMatrix{T},
+        q :: Matrix{T},
+        itau :: Vector{I},
+        R_lat :: Matrix{T}; buffer = default_buffer())
+    nq = size(q, 2)
+    ndims = size(q, 1)
+    nat_sc = size(matrix_r, 1) ÷ ndims
+    nat = size(matrix_q, 1) ÷ ndims
+
+    matrix_r .= T(0.0) 
+
+    @no_escape buffer begin
+        ΔR⃗ = @alloc(T, ndim)
+
+        phase_i = Complex(T)(2π * 1im)
+
+        for iq in 1:nq
+            for k_i in 1:nat
+                k = ndims*(k_i - 1) + k_α
+                @simd for h_i in 1:nat_sc
+                    @views ΔR⃗ .= R_lat[:, k_i]
+                    @views ΔR⃗ .-= R_lat[:, h_i]
+                    @views q_dot_R = ΔR⃗' * q[:, iq]
+
+                    h_i_uc = itau[h_i]
+
+                    exp_factor = exp(phase_i * q_dot_R)
+                    @views matrix_r[ndims*(h_i - 1) : ndims * h_i, ndims*(k_i - 1) : ndims*k_i, iq] .= matrix_q[ndims*(h_i_uc - 1) : ndims * h_i_uc, ndims*(k_i - 1) : ndims*k_i, iq]
+                    @views matrix_r[ndims*(h_i - 1) : ndims * h_i, ndims*(k_i - 1) : ndims*k_i, iq] .*= exp_factor
+                end
+            end
+        end
+        nothing
+    end
 end
 
